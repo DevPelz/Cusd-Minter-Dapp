1619a3078f3eb9ab171342fc23f1afaaa4259cda
fedesuarez16 <fedesuarez16@gmail.com>
2023-04-12 15:06:46 -0300

    smart contract improvemnets

--- contracts/ColabToken.sol
+++ contracts/ColabToken.sol
@@ -18,6 +18,13 @@ contract ColabToken is ERC721, ERC721URIStorage, Ownable {
     /// @notice Mints a new token and assign the function caller to be the owner
     /// @param uri Token metadata URI
     function safeMint(string memory uri) public {
+     require(bytes(uri).length > 0, "URI must not be empty"); // validate input
+
+         uint256 tokenId = _tokenIdCounter.current();
+        _safeMint(msg.sender, tokenId);
+        _setTokenURI(tokenId, uri);
+        _tokenIdCounter.increment();
+
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(msg.sender, tokenId);
@@ -45,4 +52,16 @@ contract ColabToken is ERC721, ERC721URIStorage, Ownable {
     {
         return super.tokenURI(tokenId);
     }
+
+    /// @notice Transfer ownership of a token from one address to another
+    /// @param from Address of the current owner of the token
+    /// @param to Address of the new owner of the token
+    /// @param tokenId ID of the token to be transferred
+function transferToken(address from, address to, uint256 tokenId) public {
+    require(_isApprovedOrOwner(_msgSender(), tokenId), "ColabToken: transfer caller is not owner nor approved");
+    require(to != address(0), "ColabToken: transfer to the zero address");
+
+    _transfer(from, to, tokenId);
+}
+
 }

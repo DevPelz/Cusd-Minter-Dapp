652eeee340e3bbc145afeaf8c977d3c7eea3cbb2
Colin Stark <collinscodes@gmail.com>
2023-04-26 07:33:34 +0100

    Update ColabToken.sol
    😄 Hello **[pelzfx](https://github.com/pelzfx)**
    
    I have viewed your project. i made some adjustments
    
    # Fix and Improvement
    1. I added a maximum number of tokens that can be minted which was used to avoid unlimited minting and also minting beyond supply.
    2. The `tokenURI` function can throw an error if called with a token ID that does not exist.
    3. The contract does not define a fallback function. This can lead to accidental transfers of tokens to the contract, which would then be irretrievable
    4. Previously i had also added events to ensure transparency.
    
    
    # Suggestion
    The following suggestions were made along with codes to show how it should be done 
     - I added a fee to the safeMint function to incentivize users to hold the tokens and prevent spamming the minting function.
     - I added a feature to enable token holders transfer their token to other user.
    
    Other  suggestions which were not coded are
    
     - There should be an implementation for token holders to lock their token which would be for staking purpose and give them governance right 
    
    # CONCLUSION 
    In conclusion i love to say this is an amazing project with prospects and i will like to see it grow further

--- contracts/ColabToken.sol
+++ contracts/ColabToken.sol
@@ -10,18 +10,26 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 /// @notice NFT contract for celo 
 contract ColabToken is ERC721, ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
+    uint256 public transferFee = 0.1 ether;
 
     Counters.Counter private _tokenIdCounter;
+    uint256 private constant MAX_TOKENS = 10000;
 
     event TokenMinted(address indexed owner, uint256 indexed tokenId, string uri);
     event TokenTransferred(address indexed from, address indexed to, uint256 indexed tokenId);
+    
+     uint256 public mintingFee;
 
-    constructor() ERC721("ColabToken", "COT") {}
+    constructor() ERC721("ColabToken", "COT") {
+        mintingFee = _mintingFee;
+    }
 
     /// @notice Mints a new token and assign the function caller to be the owner
     /// @param uri Token metadata URI
     function safeMint(string calldata uri) public {
+        require(msg.value >= mintingFee, "Insufficient fee");
         require(bytes(uri).length > 0, "Empty uri");
+         require(_tokenIdCounter.current() < MAX_TOKENS, "Maximum number of tokens minted");
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(msg.sender, tokenId);
@@ -34,8 +42,28 @@ contract ColabToken is ERC721, ERC721URIStorage, Ownable {
     function tokenCounter() public view returns (uint256) {
         return  _tokenIdCounter.current();
     }
+    
+    /// @notice Transfer a token to another user
+    /// @param to The address of the user to receive the token
+    /// @param tokenId The ID of the token to transfer
+    function transfer(address to, uint256 tokenId) public payable {
+        require(msg.value >= transferFee, "Insufficient transfer fee");
 
-    // The following functions are overrides required by Solidity.
+        require(_isApprovedOrOwner(msg.sender, tokenId), "Not approved to transfer");
+        _transfer(msg.sender, to, tokenId);
+
+        if (msg.value > transferFee) {
+            payable(msg.sender).transfer(msg.value - transferFee);
+        }
+    }
+    
+    /// @notice Set the transfer fee for tokens
+    /// @param fee The new transfer fee amount
+    function setTransferFee(uint256 fee) public onlyOwner {
+        transferFee = fee;
+    }
+
+    /// The following functions are overrides required by Solidity.
 
     function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
         super._burn(tokenId);
@@ -47,11 +75,20 @@ contract ColabToken is ERC721, ERC721URIStorage, Ownable {
         override(ERC721, ERC721URIStorage)
         returns (string memory)
     {
+        require(_exists(tokenId), "Token does not exist");
         return super.tokenURI(tokenId);
     }
 
     function transferFrom(address from, address to, uint256 tokenId) public override {
         super.transferFrom(from, to, tokenId);
         emit TokenTransferred(from, to, tokenId);
+    }
+    
+    fallback() external payable {
+        revert("This contract does not accept this token/payment.");
+    }
+
+    receive() external payable {
+        revert("This contract does not accept this token/payment .");
     }
 }
